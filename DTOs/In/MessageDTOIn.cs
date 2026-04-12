c503d19c0659e89c1197220bb64726005c49d6a5
Luca Panariello <luca.panariello@gmail.com>
2026-04-12 20:27:57 +0200

    aggiunto campo Name al messaggio

--- DTOs/In/MessageDTOIn.cs
+++ DTOs/In/MessageDTOIn.cs
@@ -5,6 +5,8 @@ namespace BEPersonal.DTOs.In
     public class MessageDTOIn
     {
         // public int UserId { get; set; }
+
+        public required string Name { get; set; }
         public required string Email { get; set; }
         public required string Content { get; set; }
     }
--- Entities/Message.cs
+++ Entities/Message.cs
@@ -3,6 +3,7 @@
     public class Message
     {
         public Guid Id { get; set; }
+        public required string Name { get; set; } = string.Empty;
         public required string Email { get; set; } = string.Empty;
         public required string Content { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
